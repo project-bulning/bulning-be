8f10bbca99ca322daafd86bf3ba937a2cfab2091
cla6shade <cla6shade@gmail.com>
2025-01-01 22:36:45 +0900

    Feat: 벌레 이미지 업로드 기능 구현

--- src/controllers/bugPostController.ts
+++ src/controllers/bugPostController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
-//import { createPost} from '../services/bugPostService';
+import { sendError } from '@/utils/response';
+import { uploadToS3 } from '@/utils/upload';
+import { CreateBugImageResponse } from '@/dto/reportDto';
 
 // 벌레 정보 입력
-export const createBugPost = async (req: Request, res: Response): Promise<void> => {
+export const createBugPost = async (req: Request, res: Response) => {
     console.log("Request body:", req.body);
   try {
     const { title, bug_type, bug_size, equipment, note, price } = req.body || {};
@@ -15,6 +17,17 @@ export const createBugPost = async (req: Request, res: Response): Promise<void> 
 };
 
 // 벌레 사진 입력
-export const uploadBugImage = async (req: Request, res: Response): Promise<void> => {
-
-};
+export const uploadBugImage = async (req: Request, res: Response<CreateBugImageResponse>) => {
+  if(! req.file) {
+    return sendError(res, '파일이 업로드되지 않았습니다.');
+  }
+  try {
+    const url = await uploadToS3(req.file);
+    res.json({
+      image_url: url,
+    });
+  } catch(e) {
+    console.error(e);
+    return sendError(res, '파일 업로드에 실패했습니다.');
+  }
+}
