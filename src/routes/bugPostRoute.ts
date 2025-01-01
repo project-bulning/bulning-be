5a840d88467c6c73d81eb40f8fbad750356a9596
cla6shade <cla6shade@gmail.com>
2025-01-01 22:36:04 +0900

    Refactor: path alias 반영

--- src/routes/bugPostRoute.ts
+++ src/routes/bugPostRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBugPost, uploadBugImage } from '../controllers/bugPostController';
+import { createBugPost, uploadBugImage } from '@/controllers/bugPostController';
+import { uploadImageMiddleware } from '@/middlewares/fileUploadMiddleware';
 
 const router = express.Router();
 
 router.post('/bug', createBugPost);  // 벌레 정보 입력
-router.post('/bug/image', uploadBugImage);  // 벌레 사진 입력
+router.post('/bug/image', uploadImageMiddleware.single('image'), uploadBugImage);  // 벌레 사진 입력
 
 export const bugPostRoute = router;
