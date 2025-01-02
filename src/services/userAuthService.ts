6d101972c270eb513d84303192a741859d228cd7
cla6shade <cla6shade@github.com>
2025-01-02 16:22:31 +0900

    Refactor: 미구현 내용 주석처리

--- src/services/userAuthService.ts
+++ src/services/userAuthService.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
-import { KaKaoUserDTO, RegistrationRequestBody } from '../dto/userDto';
+import { KaKaoUserDTO, RegistrationRequestBody } from '@/dto/userDto';
 import { PrismaClient } from '@prisma/client';
-import { Request, Response } from 'express';
+import type { Response } from 'express';
+import type { AuthenticatedRequest } from '@/types/express';
 
 const prisma = new PrismaClient();
 
@@ -39,17 +40,12 @@ export const handleUserLogin = async (user: KaKaoUserDTO) => {
   }
 };
 
-// 확장된 Request 타입 정의
-interface AuthenticatedRequest extends Request {
-  userId: string;
-}
-
 //회원 가입 -> 사용자 정보 추가 로직
 export const handlUserInfoLogic =  async (
   req: AuthenticatedRequest,
   res: Response
 ) => {
-    const {  gender, ageRange, phoneNumber, location, termsAccepted } = req.body;
+    // const {  gender, ageRange, phoneNumber, location, termsAccepted } = req.body;
 
     // //약관 동의 필드 필수 true
     // if (!termsAccepted) {
@@ -60,24 +56,24 @@ export const handlUserInfoLogic =  async (
     // }
 
     //userId를 조회해서 db에 접근한 후 req.body에 있는 정보 db에 추가 저장하는 코드
-    try {
-      await prisma.user.update({
-        where: { id: parseInt(req.userId) },
-        data: {
-          gender,
-          age_group: ageRange,
-          phone_number: phoneNumber,
-          location,
-          terms_accepted: termsAccepted ? 1 : 0,
-          updated_at: new Date(),
-        },
-      });
+    // try {
+    //   await prisma.user.update({
+    //     where: { id: parseInt(req.userId) },
+    //     data: {
+    //       gender,
+    //       age_group: ageRange,
+    //       phone_number: phoneNumber,
+    //       location,
+    //       terms_accepted: termsAccepted ? 1 : 0,
+    //       updated_at: new Date(),
+    //     },
+    //   });
 
-      res.status(200).json({ message: 'User registration details updated successfully' });
-    } catch (error) {
-      console.error('Failed to update user registration details:', error);
-      res.status(500).json({ message: 'Failed to update user registration details' });
-    }
+    //   res.status(200).json({ message: 'User registration details updated successfully' });
+    // } catch (error) {
+    //   console.error('Failed to update user registration details:', error);
+    //   res.status(500).json({ message: 'Failed to update user registration details' });
+    // }
 
 };
 
