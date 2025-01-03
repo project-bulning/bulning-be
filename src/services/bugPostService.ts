fe4f841814e34e83d72ad3242b7049ee6919f21b
cla6shade <cla6shade@gmail.com>
2025-01-03 20:35:56 +0900

    Feat: price가 음수일 경우 예외처리

--- src/services/bugPostService.ts
+++ src/services/bugPostService.ts
@@ -8,6 +8,9 @@ export const createBugReport = (data: CreateBugReportRequestBody, user: User) =>
   if(data.bug_image_url && ! isValidS3Url(data.bug_image_url)) {
     throw new Error('올바르지 않은 이미지 URL입니다.');
   }
+  if(data.price < 0) {
+    throw new Error('값이 0보다 작을 수 없습니다.');
+  }
   const creationInput = {
     ...data,
     user: {
