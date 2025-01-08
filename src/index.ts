9220ffa8dae3449ed8872fb788bacaf19e072a17
cla6shade <cla6shade@github.com>
2025-01-08 14:32:41 +0900

    Feat: production 모드에서 SSL 적용

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,14 @@ import { bugPostRoute } from './routes/bugPostRoute';
 import { bugListRoute } from './routes/bugListRoute';
 import { userRoute } from '@/routes/user';
 import cors from 'cors';
+import fs from 'node:fs';
+import https from 'https';
 
 const app = express();
-const PORT = 3000;
+const DEV_PORT = 3000;
+const PRODUCTION_PORT = 443;
+const API_PREFIX = '/api'
+
 app.use(express.json({ limit: '1mb' }));
 app.use(cors({
   origin: '*'
@@ -19,12 +24,25 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
 
-app.use('',kakaoLoginRoute);
-app.use('',bugPostRoute);
-app.use('',bugListRoute);
-app.use('',userRoute);
+app.use(API_PREFIX, kakaoLoginRoute);
+app.use(API_PREFIX, bugPostRoute);
+app.use(API_PREFIX, bugListRoute);
+app.use(API_PREFIX, userRoute);
 
 // 서버 시작
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if(process.env.NODE_ENV === 'production') {
+  const keyPath = process.env.SSL_PRIVKEY as string;
+  const caPath = process.env.SSL_CERT as string;
+  const certPath = process.env.SSL_FULLCHAIN as string;
+  const securityConfig = {
+    key: fs.readFileSync(keyPath),
+    ca: fs.readFileSync(caPath),
+    cert: fs.readFileSync(certPath),
+  };
+  const server = https.createServer(securityConfig, app);
+  server.listen(PRODUCTION_PORT, () => `Server is running on ${process.env.BASE_URL}:${PRODUCTION_PORT}`);
+} else {
+  app.listen(DEV_PORT, () => {
+    console.log(`Server is running on http://localhost:${DEV_PORT}`);
+  });
+}
