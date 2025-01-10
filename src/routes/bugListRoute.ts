21d93f8e2e0676f3a948397342109cbb00a8edbd
nyeong-deo <nyeong-deo@github.com>
2025-01-10 14:07:42 +0900

    Rename: api 경로 수정정

--- src/routes/bugListRoute.ts
+++ src/routes/bugListRoute.ts
@@ -4,7 +4,7 @@ import { authenticateToken } from '@/middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.get('/bug/list', getBugReportList);  // 사냥 리스트 조회
-router.get('/bug/list/:id', authenticateToken, getBugReportDetail);  // 사냥 상세 정보 조회
+router.get('/bug-reports', getBugReportList);  // 사냥 리스트 조회
+router.get('/bug-reports/:id', authenticateToken, getBugReportDetail);  // 사냥 상세 정보 조회
 
 export const bugListRoute = router;
