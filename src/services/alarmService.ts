af21e9ab82cce33bd80882d5f39a8f61b7f3a450
nyeong-deo <nyeong-deo@github.com>
2025-01-13 15:23:51 +0900

    Feat: 헌터의 지원 핼피에게 알림 보내기

--- src/services/alarmService.ts
+++ src/services/alarmService.ts
@@ -1,5 +1,6 @@
 import prisma from '@/utils/database';
 import type { User } from '@prisma/client';
+import { messaging } from '@/utils/firbase';
 
 export const registerTokenService = async (fcmToken: string, user: User) => {
     try {
@@ -12,3 +13,39 @@ export const registerTokenService = async (fcmToken: string, user: User) => {
       throw new Error("FCM 토큰 저장 시 오류가 발생했습니다");
     }
   };
+
+export const sendAlarmService = async (reportId: number, user: User) => {
+  try {
+    // BugReport의 status를 'PENDING'으로 업데이트
+    await prisma.bugReport.update({
+      where: { id: reportId },
+      data: { status: "PENDING" },
+    });
+
+    // BugReport와 연결된 사용자 정보 가져오기
+    const bugReport = await prisma.bugReport.findUnique({
+      where: { id: reportId },
+      include: { user: true },
+    });
+
+    if (!bugReport || !bugReport.user || !bugReport.user.fcm_token) {
+      throw new Error("No user or FCM token found for this report");
+    }
+
+    const message = {
+      notification: {
+        title: "헌터의 지원",
+        body: `${user.name}님이 사냥을 지원하셨습니다 수락하시겠습니까?`,
+      },
+      token: bugReport.user.fcm_token,
+    };
+
+    // FCM을 통해 알림 전송
+    await messaging.send(message);
+
+    console.log(`Notification sent to user with ID: ${user.id}`);
+  } catch (error) {
+    console.error("Error in sendAlarmService:", error);
+    throw new Error("알림 전송 중 오류가 발생했습니다");
+  }
+};
