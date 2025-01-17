e291355e05ed22f4fc794dcf8cfb8b0edf242fc3
cla6shade <cla6shade@github.com>
2025-01-17 10:21:28 +0900

    Feat: 사용자 정보 조회 시 매치 정보를 함께 전송하도록 구현

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,16 +1,26 @@
 import { AuthenticatedRequest } from '@/types/express';
 import { sendError } from '@/utils/response';
 import { Response } from 'express';
-import { User } from '@prisma/client';
 import { StatusCodes } from 'http-status-codes';
+import prisma from '@/utils/database';
+import { Match, User } from '@prisma/client';
 
 export const getMyInfo = async(req: AuthenticatedRequest, res: Response) => {
   if(! req.user) {
     sendError(res, '로그인된 사용자가 아닙니다.', StatusCodes.UNAUTHORIZED);
     return;
   }
-  const ret: Partial<User> = {
-    ...req.user
+  const match = await prisma.match.findFirst({
+    where: {
+      OR: [
+        { helper: req.user },
+        { hunter: req.user },
+      ]
+    }
+  });
+  const ret: Partial<User> & { match: Match | null } = {
+    ...req.user,
+    match,
   };
   delete ret.kakao_id;
   res.json(ret);
