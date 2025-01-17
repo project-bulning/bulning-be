8e46961d7ef895d951e20c90720850e9d844930a
cla6shade <cla6shade@gmail.com>
2025-01-17 13:07:08 +0900

    Feat: 서버가 네트워크 세션을 저장하도록 변경

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,7 +6,9 @@ import { ExtendedJWTPayload } from '@/dto/authDto';
 import prisma from '@/utils/database';
 import { User } from '@prisma/client';
 import { closeSocket } from '@/utils/response';
+import { NetworkSessions } from '@/types/websocket';
 
+const networkSessions: NetworkSessions = {};
 export const handleSocketConnection = async(ws: WebSocket, req: http.IncomingMessage) => {
   const token = req.url?.split('?token=')[1];
   if(! token) {
@@ -30,6 +32,7 @@ export const handleSocketConnection = async(ws: WebSocket, req: http.IncomingMes
         id: decoded.id,
       },
     }) as User;
+    networkSessions[decoded.id] = ws;
     ws.on('message', (message) => handleMessage(user, message.toString(), ws))
   } catch(e) {
     console.error(e);
