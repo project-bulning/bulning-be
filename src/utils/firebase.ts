c2f629d15dbe54252ef5ad5eba00cb770e6157f6
cla6shade <cla6shade@github.com>
2025-01-27 13:40:38 +0900

    Fix: typo and hard-coded firebase filename

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 import * as fs from 'fs';
 
-const serviceAccount = JSON.parse(fs.readFileSync('firebase-serviceAccountKey.json').toString());
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.FIREBASE_JSON as string).toString());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
